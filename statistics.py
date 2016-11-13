88f4d00823f520a69a275fef29375399e2d2a03b
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-11-14 00:34:44 +0100

    Bag of top 100 words

--- statistics.py
+++ statistics.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import time
 import numpy as np
 import pandas as pd
-import sys
 import glob
+import operator
 
 categories_file = "categories.tsv"
 
@@ -18,8 +19,26 @@ frame = pd.concat(list_)
 frame = frame.values
 print("Czas wczytywania w s: " + str(time.time()-t0))
 
+t0 = time.time()
 categories = pd.read_csv(categories_file, sep="\t", header=None, skiprows=[0,1]).values
+print("Czas wczytywania w s: " + str(time.time()-t0))
+
 print(categories[:,0].size)
 print(frame[:,0].size)
 
-
+# Get bags of 100 most popular words
+titles = frame[:,1]
+words = []
+for title in titles:
+    title = title.replace(',', '')
+    string_words = title.split()
+    for word in string_words:
+        if(len(word) > 2):
+            words.append(word)
+classes = {}
+for value in set(words):
+    classes[value] = 0
+for value in words:
+    classes[value] += 1
+sorted_words = sorted(classes.items(), key=operator.itemgetter(1))[-100:]
+print(len(sorted_words))
